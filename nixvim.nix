fe9a0a3247d173d3c1bea62a531c60185360e10e
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-12-25 21:39:22 +1000

    Update nixvim.nix

--- nixvim.nix
+++ nixvim.nix
@@ -123,7 +123,7 @@
     autoread = true;
     undofile = true;
     undodir = "/tmp/.vim-undo-dir";
-    backupdir = "~/nvim-history";
+    backupdir = "/.neovim-history";
   };
 
   extraPlugins = with pkgs; [
