ccf262ba0e84fd442b521d8cd4ce18438f3806ae
Chelsea Wilkinson <mail@chelseawilkinson.me>
2025-06-30 19:48:39 +1000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Example nix-darwin system flake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.05";
