a3733facfaa63abb41929ce5bfbd1214e7e7418f
Chelsea Wilkinson <mail@chelseawilkinson.me>
2025-07-11 17:03:55 +1000

    neaten flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
     stylix.url = "github:danth/stylix/release-25.05";
+
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     nixvim.url = "github:nix-community/nixvim/nixos-25.05";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
+
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
     zjstatus.url = "github:dj95/zjstatus";
-    nix-modules = {
-      url = "github:chelsea6502/nix-modules";
-      #url = "path:/Users/chelsea/modules"; # dev mode
-      flake = false;
-    };
+
+    nix-modules.url = "github:chelsea6502/nix-modules";
+    #nix-modules.url = "path:/Users/chelsea/modules"; # dev mode
+    nix-modules.flake = false;
   };
+
   outputs =
     inputs@{
       self,
@@ -30,7 +36,7 @@
     }:
     {
       darwinConfigurations."Chelseas-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-        specialArgs = { inherit nix-modules; };
+        specialArgs = { inherit (inputs) nix-modules; };
         modules = [
           nix-homebrew.darwinModules.nix-homebrew
           {
